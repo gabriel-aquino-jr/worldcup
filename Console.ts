ac40e6319b537f9b2c5b719930e368939695e618
galzer <galzer@outlook.com>
2022-12-16 04:49:24 -0400

    added tournament class | error in method find

--- Console.ts
+++ Console.ts
@@ -1,11 +1,25 @@
-export class Team {
+/**
+ * @author Gabriel Aquino
+ * @date Dec/16/2022
+ */
+
+/**
+ * Team information
+ */
+export class Team {    
     constructor( public name: string ) {}
 }
 
+/**
+ * Half side of each match, with team and score of the team
+ */
 export class SideMatch {
     constructor( public team: Team, public score: number) {}
 }
 
+/**
+ * Two side matches, that represents the match itself
+ */
 export class Match {
     constructor( private game: SideMatch[] ) { }
 
@@ -13,12 +27,12 @@ export class Match {
         this.game[team].score += goal;
     }
 
-    winner() : string {
+    winner() : number {
         if( this.game[0].score > this.game[1].score )
-            return this.game[0].team.name;
+            return 0;
         if( this.game[0].score < this.game[1].score )
-            return this.game[1].team.name;            
-        return 'tie';            
+            return 1;            
+        return -1;            
     }
 
     toString() {
@@ -26,50 +40,130 @@ export class Match {
     }
 }
 
+/**
+ * Team stats, with points, (todo: victories, draws, loses, goals forward, goals against, goals difference)
+ */
 export class TeamStats {
-    public team: Team;
-    public point: number;
+
+    constructor(public team: Team, public point: number) {}
 }
 
 export class Tournament {
-    public table: TeamStats[];
+    private teams: Team[];
+    private table: TeamStats[];
+    private fixtures: Match[];
 
-    
+    constructor() {}
+
+    public addTeam(teamName: string) {
+        let team: Team = new Team(teamName);
+        this.teams.push(team);
+        let teamStats = new TeamStats(team, 0);
+        this.table.push(teamStats);
+    }
+
+    /**
+     * Get the team
+     * #question how to return only the Team type? It's requiring to define undefined
+     * @param teamName 
+     * @returns 
+     */
+    public getTeam(teamName :string) : Team {
+        try {
+            let teamFound = new Team('temp');
+            teamFound = this.teams.find(t => t.name = teamName);
+            if (teamFound === undefined) {
+                throw new TypeError('The value was promised to always be there!');
+              }
+            return teamFound;   
+        } catch (error) {
+            throw error;
+        }
+            
+    }
+
+    public addMatch(team1: Team, team2: Team) {
+        let sideMatch1: SideMatch = new SideMatch(team1, 0);
+        let sideMatch2: SideMatch = new SideMatch(team2, 0);
+
+        var game1: SideMatch[] = [];
+
+        game1.push(sideMatch1);
+        game1.push(sideMatch2);
+
+        let match1 = new Match( game1 );
+
+        this.fixtures.push(match1);
+    }
+
+    public addMatchResults(matchIndex: number, score1: number, score2: number) {
+        this.fixtures[matchIndex].addScore(0, score1);
+        this.fixtures[matchIndex].addScore(1, score2);
+        this.calculateMatchPoints(matchIndex);
+    }
+
+    private calculateMatchPoints(matchIndex: number) {
+        let results : number = this.fixtures[matchIndex].winner();
+        if (results !== -1)
+        {
+            console.log(this.fixtures[matchIndex][results]);             
+        }       
+        
+    }
+
+    public getMatch(index: number) : Match {
+        return this.fixtures[index];
+    }
+
 }
 /**
  * testing
  */
 
-let team1 = new Team('Brazil');
-let team2 = new Team('Canada');
+// let team1 = new Team('Brazil');
+// let team2 = new Team('Canada');
 
-console.log(team1);
-console.log(team2);
+// console.log(team1);
+// console.log(team2);
 
-let sideMatch1 = new SideMatch(team1, 0);
-let sideMatch2 = new SideMatch(team2, 0);
+// let sideMatch1 = new SideMatch(team1, 0);
+// let sideMatch2 = new SideMatch(team2, 0);
 
-console.log(sideMatch1);
-console.log(sideMatch2);
+// console.log(sideMatch1);
+// console.log(sideMatch2);
 
-var game1: SideMatch[] = [];
+// var game1: SideMatch[] = [];
 
-game1.push(sideMatch1);
-game1.push(sideMatch2);
+// game1.push(sideMatch1);
+// game1.push(sideMatch2);
 
-let match1 = new Match( game1 );
+// let match1 = new Match( game1 );
 
-console.log(match1);
+// console.log(match1);
 
-match1.toString();
-console.log(`The Winner is ${match1.winner()}`);
+// match1.toString();
+// console.log(`The Winner is ${match1.winner()}`);
 
-match1.addScore(0, 2);
+// match1.addScore(0, 2);
 
-match1.toString();
-console.log(`The Winner is ${match1.winner()}`);
+// match1.toString();
+// console.log(`The Winner is ${match1.winner()}`);
 
 
-match1.addScore(1, 3);
-match1.toString();
-console.log(`The Winner is ${match1.winner()}`);
+// match1.addScore(1, 3);
+// match1.toString();
+// console.log(`The Winner is ${match1.winner()}`);
+
+
+let worldCup2022: Tournament = new Tournament();
+
+worldCup2022.addTeam('Brazil');
+worldCup2022.addTeam('Canada');
+
+worldCup2022.addMatch(worldCup2022.getTeam('Brazil'),worldCup2022.getTeam('Canada'));
+
+worldCup2022.addMatchResults(0, 2, 0);
+
+
+
+
--- console.js
+++ console.js
@@ -1,6 +1,13 @@
 "use strict";
+/**
+ * @author Gabriel Aquino
+ * @date Dec/16/2022
+ */
 exports.__esModule = true;
-exports.Match = exports.SideMatch = exports.Team = void 0;
+exports.Tournament = exports.TeamStats = exports.Match = exports.SideMatch = exports.Team = void 0;
+/**
+ * Team information
+ */
 var Team = /** @class */ (function () {
     function Team(name) {
         this.name = name;
@@ -8,6 +15,9 @@ var Team = /** @class */ (function () {
     return Team;
 }());
 exports.Team = Team;
+/**
+ * Half side of each match, with team and score of the team
+ */
 var SideMatch = /** @class */ (function () {
     function SideMatch(team, score) {
         this.team = team;
@@ -16,6 +26,9 @@ var SideMatch = /** @class */ (function () {
     return SideMatch;
 }());
 exports.SideMatch = SideMatch;
+/**
+ * Two side matches, that represents the match itself
+ */
 var Match = /** @class */ (function () {
     function Match(game) {
         this.game = game;
@@ -25,10 +38,10 @@ var Match = /** @class */ (function () {
     };
     Match.prototype.winner = function () {
         if (this.game[0].score > this.game[1].score)
-            return this.game[0].team.name;
+            return 0;
         if (this.game[0].score < this.game[1].score)
-            return this.game[1].team.name;
-        return 'tie';
+            return 1;
+        return -1;
     };
     Match.prototype.toString = function () {
         console.log("".concat(this.game[0].team.name, " ").concat(this.game[0].score, " vs ").concat(this.game[1].score, " ").concat(this.game[1].team.name));
@@ -37,26 +50,96 @@ var Match = /** @class */ (function () {
 }());
 exports.Match = Match;
 /**
+ * Team stats, with points, (todo: victories, draws, loses, goals forward, goals against, goals difference)
+ */
+var TeamStats = /** @class */ (function () {
+    function TeamStats(team, point) {
+        this.team = team;
+        this.point = point;
+    }
+    return TeamStats;
+}());
+exports.TeamStats = TeamStats;
+var Tournament = /** @class */ (function () {
+    function Tournament() {
+    }
+    Tournament.prototype.addTeam = function (teamName) {
+        var team = new Team(teamName);
+        this.teams.push(team);
+        var teamStats = new TeamStats(team, 0);
+        this.table.push(teamStats);
+    };
+    /**
+     * Get the team
+     * #question how to return only the Team type? It's requiring to define undefined
+     * @param teamName
+     * @returns
+     */
+    Tournament.prototype.getTeam = function (teamName) {
+        try {
+            var teamFound = new Team('temp');
+            teamFound = this.teams.find(function (t) { return t.name = teamName; });
+            if (teamFound === undefined) {
+                throw new TypeError('The value was promised to always be there!');
+            }
+            return teamFound;
+        }
+        catch (error) {
+            throw error;
+        }
+    };
+    Tournament.prototype.addMatch = function (team1, team2) {
+        var sideMatch1 = new SideMatch(team1, 0);
+        var sideMatch2 = new SideMatch(team2, 0);
+        var game1 = [];
+        game1.push(sideMatch1);
+        game1.push(sideMatch2);
+        var match1 = new Match(game1);
+        this.fixtures.push(match1);
+    };
+    Tournament.prototype.addMatchResults = function (matchIndex, score1, score2) {
+        this.fixtures[matchIndex].addScore(0, score1);
+        this.fixtures[matchIndex].addScore(1, score2);
+        this.calculateMatchPoints(matchIndex);
+    };
+    Tournament.prototype.calculateMatchPoints = function (matchIndex) {
+        var results = this.fixtures[matchIndex].winner();
+        if (results !== -1) {
+            console.log(this.fixtures[matchIndex][results]);
+        }
+    };
+    Tournament.prototype.getMatch = function (index) {
+        return this.fixtures[index];
+    };
+    return Tournament;
+}());
+exports.Tournament = Tournament;
+/**
  * testing
  */
-var team1 = new Team('Brazil');
-var team2 = new Team('Canada');
-console.log(team1);
-console.log(team2);
-var sideMatch1 = new SideMatch(team1, 0);
-var sideMatch2 = new SideMatch(team2, 0);
-console.log(sideMatch1);
-console.log(sideMatch2);
-var game1 = [];
-game1.push(sideMatch1);
-game1.push(sideMatch2);
-var match1 = new Match(game1);
-console.log(match1);
-match1.toString();
-console.log("The Winner is ".concat(match1.winner()));
-match1.addScore(0, 2);
-match1.toString();
-console.log("The Winner is ".concat(match1.winner()));
-match1.addScore(1, 3);
-match1.toString();
-console.log("The Winner is ".concat(match1.winner()));
+// let team1 = new Team('Brazil');
+// let team2 = new Team('Canada');
+// console.log(team1);
+// console.log(team2);
+// let sideMatch1 = new SideMatch(team1, 0);
+// let sideMatch2 = new SideMatch(team2, 0);
+// console.log(sideMatch1);
+// console.log(sideMatch2);
+// var game1: SideMatch[] = [];
+// game1.push(sideMatch1);
+// game1.push(sideMatch2);
+// let match1 = new Match( game1 );
+// console.log(match1);
+// match1.toString();
+// console.log(`The Winner is ${match1.winner()}`);
+// match1.addScore(0, 2);
+// match1.toString();
+// console.log(`The Winner is ${match1.winner()}`);
+// match1.addScore(1, 3);
+// match1.toString();
+// console.log(`The Winner is ${match1.winner()}`);
+var worldCup2022 = new Tournament();
+worldCup2022.addTeam('Brazil');
+worldCup2022.addTeam('Canada');
+worldCup2022.addMatch(worldCup2022.getTeam('Brazil'), worldCup2022.getTeam('Canada'));
+worldCup2022.addMatchResults(0, 2, 0);
